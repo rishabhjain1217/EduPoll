9ed53ae07661ae990435a801c9fbda103bd648a2
Steve Johnson <stephenjohnson150@gmail.com>
2021-09-27 00:16:10 -0400

    Initial commit.

--- screens/CreateAccount.js
+++ screens/CreateAccount.js
@@ -3,19 +3,16 @@ import { Text, View, StyleSheet, Image } from 'react-native';
 
 import Constants from 'expo-constants';
 
-// You can import from local files
-// import AssetExample from './components/AssetExample';
-import ProfSign from './screens/ProfSign'
-import CreateAccount from './screens/CreateAccount'
-
-// or any pure javascript modules available in npm
-//import { Card } from 'react-native-paper';
 
 export default function App() {
   return (
-    //<ProfSign/>
-    <CreateAccount/>
-  );
+     <View style={styles.container}>
+        <Image source={require("../assets/book_logo.png")} />
+        <Text style={styles.welcome_message} Welcome to EduPoll/>
+      <View style={styles.activate}>
+      </View>     
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -24,16 +21,21 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems:'center',
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#6BC7A6',
+    backgroundColor: '#F2F2F2',
   
   },
-  activate:{
+  welcome_message: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  /*activate:{
     height:556,
     marginTop: 20,
     width: "100%",
     borderRadius:20,
-    backgroundColor: "white"
-  },
+    backgroundColor: "#F2F2F2"
+  },*/
   paragraph: {
     margin: 24,
     fontSize: 18,
