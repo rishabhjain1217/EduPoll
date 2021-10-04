160912995427e735518c6c4c7dcb5f1c7397913b
Rishabh Jain <rj9crg@virginia.edu>
2021-10-04 14:22:40 -0400

    HomeScreen addition

---
+++ screens/HomeScreen.js
@@ -0,0 +1,93 @@
+// You can import from local files
+import AssetExample from './components/AssetExample';
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
+
+import * as React from 'react';
+import { Text, View, Button, StyleSheet, Image, TextInput, TouchableWithoutFeedback, Keyboard} from 'react-native';
+
+import Constants from 'expo-constants';
+
+
+export default function App() {
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+     <View style={styles.container}>
+      <View style={styles.top_container}>
+        <Text style={styles.top_message}>EDUPOLL HOME</Text>
+      </View>
+      <Image source={require("assets/home.png")} />
+        <View style={styles.activate}>
+          <Text style={styles.welcome_message}>Welcome to our App!</Text>
+          <View style={styles.button_container}>
+            <Button title='Take Quiz' color="white"/>
+          </View>
+          <View style={styles.button_container}>
+            <Button title='Make Quiz' color="white"/>
+          </View>
+        </View>    
+      </View>
+    </TouchableWithoutFeedback> 
+  )
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems:'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#6BC7A6',
+  
+  },
+  top_container: {
+    height: 73,
+    width: "80%",
+    borderRadius:57,
+    backgroundColor: "#FFFFFF",
+    textAlign: "center",
+    justifyContent: "center",
+    marginTop: '5%',
+    marginBottom: '8%',
+  },
+  top_message: {
+    fontWeight: 'bold',
+    fontSize: 26,
+    textAlign: 'center',
+    paddingTop: '10%',
+    paddingBottom: '20%',
+    paddingLeft: '15%',
+    paddingRight: '15%'
+  },
+  welcome_message: {
+    fontSize: 20,
+    fontStyle: 'italic',
+    textAlign: 'center',
+    paddingTop: '10%',
+    paddingBottom: '10%',
+    paddingLeft: '15%',
+    paddingRight: '15%'
+  },
+  activate: {
+    height: "60%",
+    marginTop: 10,
+    width: "100%",
+    borderRadius:20,
+    backgroundColor: "white",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  button_container: {
+    height: "12.3%",
+    width: "80%",
+    borderRadius:20,
+    backgroundColor: "#399675",
+    textAlign: "center",
+    justifyContent: "center",
+    margin: 5,
+    marginTop: 20,
+  },
+});
